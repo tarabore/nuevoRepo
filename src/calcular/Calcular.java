3311c1bd4d29ecf77040c391c663fc10912aa886
tarabore <tarabore@gmail.com>
2025-11-11 18:23:14 -0300

    Segundo commit

--- src/calcular/Calcular.java
+++ src/calcular/Calcular.java
@@ -11,7 +11,7 @@ public class Calcular {
     }
 
     public double multiplicar(double numero1, double numero2) {
-        return numero1 * 2;
+        return numero1 * numero2;
     }
 
     public double dividir(double numero1, double numero2) {
